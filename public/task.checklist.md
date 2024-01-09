8ea6e198e517c0fd20fc4bd35f2bc6bdff6e3464
sseang <142060734+sseang@users.noreply.github.com>
2024-01-08 18:44:59 -0600

    Created and tested GET and POST routes to SQL

--- public/task.checklist.md
+++ public/task.checklist.md
@@ -1,5 +1,7 @@
  # Project Name: To-Do Task Checklist
  
+ <!-- create checklist for project -->
+
  [x] - SQL to create the database table
         - Needs to be called: weekend-to-do-app
  [x] - Create a database.sql file and add the create table SQL
@@ -8,14 +10,16 @@
         -save database from Postico to project folder
  [x] - Add sample data with insert statements
      - Add insert statements into the database.sql file
+     <!-- //should add date to complete column?? -->
+
  [] - Create a GET route in the todo.router.js file to return sample data
-        - Test the GET route with Postman
+        - Test the GET route with Postman/Postico
  [] - Create a POST route in the todo.router.js file to return sample data
-        - Test the POST route with Postman
+        - Test the POST route with Postman/Postico
  [] - Create a PUT route in the todo.router.js file to return sample data
-        - Test the PUT route with Postman
+        - Test the PUT route with Postman/Postico
  [] - Create a DELETE route in the todo.router.js file to return sample data
-        - Test the DELETE route with Postman
+        - Test the DELETE route with Postman/Postico
 
  [] - Add useEffect and Axios GET to the App.jsx
  [] - Display items on the page with .map ...
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,9 +3,59 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
+router.get('/', (req, res) => {
+
+    //query to SQL
+    const queryText = `SELECT * FROM "todo";`;
+    //pool for query and data
+    pool.query(queryText)
+    //promises
+    .then((result) => {
+        //confirm and label data
+        console.log('RESULTS:', result)
+        //send data
+        res.send(result.rows);
+    })
+    //catch to error
+    .catch((error) => {
+        //confirm and label if error
+        console.log('ERROR:', error)
+        //internal errors
+        res.sendStatus(500);
+    });
+});
 
 // POST
+router.post('/', (req, res) => {
+    //var to hold data property
+    let sqlData = req.body;
+    //query to SQL
+    const queryText = `INSERT INTO "todo" ("task", "description")
+    VALUES
+    ($1, $2);`;
+    //update data
+    const queryArgs = [
+        sqlData.task,
+        sqlData.description,
+       
+    ]
+    console.log('POST results', queryArgs);
 
+    //pool for query and data
+    pool.query(queryText, queryArgs)
+    //promises
+    .then((result) => {
+        //confirm good data
+        res.sendStatus(201);
+    })
+    //catch errors
+    .catch((error) => {
+        //confirm and label if error
+        console.log('OH SNAP!:', error);
+        //internal errors
+        res.sendStatus(500);
+    });
+});
 // PUT
 
 // DELETE
